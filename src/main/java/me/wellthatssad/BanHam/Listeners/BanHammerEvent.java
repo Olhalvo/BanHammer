3eb8e363a5264df890ecac51441ffd6eb6003a43
Well_thatssad <bernardomeloesteves1234@gmail.com>
2021-04-11 15:31:14 +0100

    Commit 002 fixing a bug

--- src/main/java/me/wellthatssad/BanHam/Listeners/BanHammerEvent.java
+++ src/main/java/me/wellthatssad/BanHam/Listeners/BanHammerEvent.java
@@ -14,35 +14,48 @@ public class BanHammerEvent implements Listener {
         Entity Damager = e.getDamager();
         Entity Banned = e.getEntity();
         if (!(Damager instanceof Player)) {
-            e.setCancelled(true);
+            return;
         } else {
             Player DamagerP = (Player) Damager;
             ItemStack mainhandItem = DamagerP.getInventory().getItemInMainHand();
-            if (DamagerP.hasPermission("BanHammer.CanUseBanhammer")) {
-                if (mainhandItem.getItemMeta().getLocalizedName().equals("ban_hammer")) {
+            if (mainhandItem.getItemMeta().getLocalizedName().equals("ban_hammer")) {
+                if (DamagerP.hasPermission("BanHammer.CanUseBanhammer")) {
                     if (!(Banned instanceof Player)) {
                         DamagerP.sendMessage(ChatColor.RED + "You can't ban a " + Banned.getType().name());
+                        e.setCancelled(true);
                     }
                     else{
                         Player BannedP = (Player) Banned;
                         if(BannedP.isOp()){
                             DamagerP.sendMessage(ChatColor.RED + "You can't ban an operator");
+                            e.setCancelled(true);
                         }else {
                             Location loc = BannedP.getLocation();
                             World world = BannedP.getWorld();
-                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "ban " + BannedP.getDisplayName());
+                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "ban " + BannedP.getDisplayName() + "You have been hit by the ban hammer, literally");
                             DamagerP.sendMessage(ChatColor.GREEN + "You have sucessfully deleted " + BannedP.getDisplayName());
                             Bukkit.broadcastMessage( ChatColor.AQUA + BannedP.getDisplayName() + ChatColor.RESET + ChatColor.GOLD + " Has been hit by the ban hammer, literally");
                             for(Player p : Bukkit.getOnlinePlayers()){
                                 p.playSound(p.getLocation(), Sound.ENTITY_CAT_DEATH, 100, 100);
                             }
                             world.strikeLightning(loc);
+                            e.setCancelled(true);
                         }
                     }
                 }
+                else{
+                    for (int i = 0; i <= 10; i++){
+                        DamagerP.getWorld().strikeLightning(DamagerP.getLocation());
+                    }
+                    DamagerP.sendMessage(ChatColor.RED + "YOU ARE NOT WORTHY");
+                    e.setCancelled(true);
+                }
+            }
+            else{
+                return;
             }
 
         }
-        e.setCancelled(true);
+
     }
 }
